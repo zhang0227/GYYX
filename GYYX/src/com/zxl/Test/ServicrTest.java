2c5bd504230cd1aa89fdd21063a93fa576b86bb2
zhangxl <zhangxl8@asiainfo.com>
2017-03-27 19:05:01 +0800

    初始化

---
+++ GYYX/src/com/zxl/Test/ServicrTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.zxl.Test;
+
+/**
+ * @author zhangxl
+ * @date： 日期：2017-3-27
+ * @version 1.0
+ */
+public class ServicrTest {
+
+	/**
+	 * 
+	 * @author zhangxl
+	 * @date： 日期：2017-3-27
+	 * @return void
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
---
+++ GYYX/src/com/zxl/service/Service.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.zxl.service;
+
+/**
+ * @author zhangxl
+ * @date£º ÈÕÆÚ£º2017-3-27
+ * @version 1.0
+ */
+public class Service {
+
+}
